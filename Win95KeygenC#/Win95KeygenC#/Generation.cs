aa7c25ebedb4cfb1df2562a7e4fc083a3de951e7
BorbasBalint <colbybot01@gmail.com>
2023-12-19 17:57:56 +0100

    Complete overhaul
    The entire code got a rewrite using OOP now instead of whatever was going on previously.

---
+++ Win95KeygenC#/Win95KeygenC#/Generation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win95KeygenC_
+{
+    internal class Generation
+    {
+        const int STARTING_DAY = 1;
+        const int TOTAL_DAYS_IN_YEAR = 365;
+        const int MIN_N_VALUE = 0;
+        const int MAX_N_VALUE = 100000;
+        const int REMAINDER_DIVISOR = 7;
+        const int RANDOMNUMBER_MIN = 10000;
+        const int RANDOMNUMBER_MAX = 100000;
+
+
+        static List<string> GeneratedKeys = new List<string>();
+        static string[] PossibleYears = { "95", "96", "97", "98", "99", "00", "01", "02", "03" };
+        static Random rnd = new Random();
+        
+        
+        
+        public static void GenerateKeys(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                string dayOfYear = rnd.Next(STARTING_DAY, TOTAL_DAYS_IN_YEAR).ToString("000");
+                //Console.WriteLine(dayOfYear);
+
+                string year = PossibleYears[rnd.Next(PossibleYears.Length)];
+                //Console.WriteLine(year);
+
+                int N = rnd.Next(MIN_N_VALUE, MAX_N_VALUE);
+                int remainder = N % REMAINDER_DIVISOR;
+                if (remainder != 0)
+                {
+                    N += REMAINDER_DIVISOR - remainder;
+                };
+                //Console.WriteLine(N);
+
+                string n_segment = "00" + N.ToString("00000");
+                //Console.WriteLine(n_segment);
+
+                int randomNumber = rnd.Next(RANDOMNUMBER_MIN, RANDOMNUMBER_MAX);
+                //Console.WriteLine(randomNumber);
+
+                string generatedString = $"{dayOfYear}{year}-OEM-{n_segment}-{randomNumber}";
+                GeneratedKeys.Add(generatedString);
+            }
+        }
+
+        public static void PrintToConsole()
+        {
+            GeneratedKeys.ForEach(x => Console.WriteLine(x));
+        }
+
+        public static void WriteToFile(string fileName)
+        {
+            File.WriteAllLines(fileName, GeneratedKeys);
+        }
+            
+    }
+}
--- Win95KeygenC#/Win95KeygenC#/Program.cs
+++ Win95KeygenC#/Win95KeygenC#/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace Win95KeygenC_
@@ -9,42 +8,30 @@ namespace Win95KeygenC_
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the amount of keys you want: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            List<string> strings = new List<string>();
-            string[] years = { "95", "96", "97", "98", "99", "00", "01", "02", "03" };
-            Random rnd = new Random();
-            for (int i = 0; i < n; i++)
+            try 
             {
-                string dayOfYear = rnd.Next(1, 366).ToString("000");
-                //Console.WriteLine(dayOfYear);
+                Console.Write("Enter the amount of keys you want: ");
+                Generation.GenerateKeys(int.Parse(Console.ReadLine()));
 
-                string year = years[rnd.Next(years.Length)];
-                //Console.WriteLine(year);
+                Generation.PrintToConsole();
 
-                int N = rnd.Next(0, 100000);
-                string formattedNumber = N.ToString("00000");
-                int remainder = Convert.ToInt32(formattedNumber) % 7;
-                if (remainder != 0)
+                Console.Write("Would you like to write the keys to a file? (Y / N): ");
+                string answer = Console.ReadLine().ToUpper();
+                if (answer.Equals("Y"))
                 {
-                    N += 7 - remainder;
-                    formattedNumber = N.ToString("00000");
-                };
-                //Console.WriteLine(N);
-
-                string n_segment = "00" + formattedNumber;
-                //Console.WriteLine(n_segment);
-
-                int randomNumber = rnd.Next(10000, 99999);
-                //Console.WriteLine(randomNumber);
-
-                string generatedString = $"{dayOfYear}{year}-OEM-{n_segment}-{randomNumber}";
-                strings.Add(generatedString);
+                    Console.Write("File name: ");
+                    Generation.WriteToFile(Console.ReadLine());
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong");
             }
 
-            strings.ForEach(x => Console.WriteLine(x));
-            File.WriteAllLines("Keys.txt", strings);
 
 
             Console.ReadKey();
